e2dbed5c7f7a76ebf81bd915cddd49518e98ca73
Chanvitha <chanvithapraween@gmail.com>
2023-11-06 18:28:59 +0530

    final

--- 19_ENG_080_P1.py
+++ 19_ENG_080_P1.py
@@ -2,19 +2,17 @@ import cv2
 import numpy as np
 import random
 
-# Global variables
+
 label_array = []
 label_conversion = {}
 
 
-# Binarize the image
 def binarize_image(image_array, threshold=130):
     binary_image = np.zeros_like(image_array)
     binary_image[image_array > threshold] = 1
     return binary_image
 
 
-# Colorize the labeled regions
 def colorize_labeled_regions(labeled_image):
     rows, columns = labeled_image.shape
     label_colors = {0: (0, 0, 0)}
@@ -30,7 +28,6 @@ def colorize_labeled_regions(labeled_image):
     return colored_image
 
 
-# Assign labels to connected components using the two-pass algorithm
 def two_pass_labeling(binary_image):
     rows, columns = binary_image.shape
 
@@ -54,7 +51,6 @@ def two_pass_labeling(binary_image):
     return binary_image
 
 
-# Determine the label for a pixel based on its neighbors
 def assign_label(neighbor_pixels):
     if all(x == 0 for x in neighbor_pixels):
         if not label_array:
@@ -76,22 +72,27 @@ def assign_label(neighbor_pixels):
             label_conversion[max_value] = min_value
             return min_value
 
+
 def main():
-    # Load the image using OpenCV. Replace "your_image.jpg" with the path to your image.
     image = cv2.imread("input_images/Sample Image 1.bmp", cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread("input_images/Sample Image 1.png", cv2.IMREAD_GRAYSCALE)
 
     # Add borders to the image
     # image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=255)
 
-    # Binarize the image
+    print(image)
     binary_image = binarize_image(image)
+    print(binary_image)
 
     labeled_image = two_pass_labeling(binary_image)
+    print(labeled_image)
     colored_image = colorize_labeled_regions(labeled_image)
+    print(colored_image)
 
+    # cv2.imwrite("output_images/labeled_image.jpg", labeled_image)
+    # cv2.imwrite("output_images/binary_image.jpg", binary_image)
     cv2.imshow("Output Colorized Image", colored_image)
 
-    # Save the output image
     cv2.imwrite("output_images/Output_Colorized_Image.jpg", colored_image)
     print("\nOutput image is saved in 'Output_Colorized_Image.jpg'")
 
